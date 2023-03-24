11c3fc2461182fbe0b38a81468a47fd88c04c235
Vinicius Soares <viniciussoares72943@gmail.com>
2023-03-24 19:36:09 -0300

    feat: implementa funcionalidade para virificar se existe um salario

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -33,29 +33,18 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
+    try:
+        if int(job['min_salary']) > int(job['max_salary']):
+            raise ValueError(
+                "minimo salrio precisa ser menor que salrio maximo"
+            )
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+        verify = int(salary) <= int(job['max_salary']) and int(
+            salary) >= int(job['min_salary'])
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+        return verify
+    except (ValueError, KeyError, TypeError):
+        raise ValueError
 
 
 def filter_by_salary_range(
