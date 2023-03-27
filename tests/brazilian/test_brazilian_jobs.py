5d3cd8fae30ce8414715208dc4f23c829ed2e1ae
Vinicius Soares <viniciussoares72943@gmail.com>
2023-03-27 13:32:11 -0300

    test: para função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    jobs = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    result_keys = {'salary', 'title', 'type'}
+    result_keys_by_func = set()
+    for job in jobs:
+        keys = job.keys()
+        for key in keys:
+            result_keys_by_func.add(key)
+    assert result_keys_by_func == result_keys
